2aea74b2c0c3e7cafe2e7f5f8177142c51d9dcaa
zenatulcobalah zenatulcobalah <zenatulcoba@gmail.com>
2024-08-27 12:03:56 +0000

    feat: initiated the project

---
+++ utils/deployed-address.ts
@@ -0,0 +1,3 @@
+export const TransparentUpgradeableProxy = '0xAc87270c183512Cd07ac46b8F05761a461B4871c'
+export const MyContractV1 = 'undefined'
+export const MyContractV2 = '0x8e25422DAB38581361D9Cdc8fcf8Dc52153086BA'
